878d5e54ebffb8f93d45823944b148d6f25290fe
danielle <danielle.costa@somosquiq.com>
2022-11-29 10:40:03 -0300

    inicio do refatoramento com service

---
+++ api/services/index.js
@@ -0,0 +1,10 @@
+const PessoasService = require('./PessoasServices')
+const NiveisService = require('./NiveisService')
+const TurmasService = require('./TurmasService')
+
+
+module.exports ={
+    PessoasService:PessoasService,
+    NiveisService:NiveisService,
+    TurmasService:TurmasService
+}
